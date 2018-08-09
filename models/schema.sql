6894622adac3ff8d9851e9914c8ad9da116adee6
John <johnsonhampson@gmail.com>
2018-08-08 21:02:25 -0400

    update JH

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,4 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS travel_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+-- Create the database
+CREATE DATABASE travel_db;
