b2842ac390799a362a29cae85efd69d002bb17d2
RAOwens <reggieowens@msn.com>
2018-08-15 19:37:09 -0400

    updated page header and footer

--- models/example.js
+++ models/example.js
@@ -1,41 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
-  var Travel = sequelize.define("Travel", {
-    employee_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate:{ len:[1,255]}
-    },
-    start_date: {
-      type:DataTypes.DATEONLY,
-      allowNull: false,
-    },
-    end_date:{
-      type:DataTypes.DATEONLY,
-      allowNull: false,
-    },
-    orgin: {
-      type:DataTypes.STRING,
-      allowNull:false,
-      validate:{len:[1,100]}
-    },
-    destination: {
-      type:DataTypes.STRING,
-      allowNull:false,
-      validate:{len:[1,100]}
-    },
-    airfare:{
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    car:{
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    hotel:{
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
+  var Example = sequelize.define("Example", {
+    text: DataTypes.STRING,
+    description: DataTypes.TEXT
   });
-  return Travel;
+  return Example;
 };
-
